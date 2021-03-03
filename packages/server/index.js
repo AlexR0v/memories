f5e64f038460ed1a9bd05c070f339d7bb603d0b1
AlexR0v <mail.ponomarev@yandex.ru>
2021-03-03 20:11:24 +0300

    feat(post): env

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,16 +2,18 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import postRoutes from './routes/post.js'
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use('/posts', postRoutes)
 
-const MONGO_URL = 'mongodb+srv://Alex:12345@memories.fbqp7.mongodb.net/memories?retryWrites=true&w=majority'
+const MONGO_URL = process.env.MONGO_URL
 
 const PORT = process.env.PORT ?? 5000
 
